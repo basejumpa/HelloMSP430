ba7fbbc891c0c43acb53bf87706dbad0b6a6e6bb
theHolgi <holger@e-gitt.net>
2015-09-03 13:15:34 +0200

    End with a green LED

--- main.c
+++ main.c
@@ -13,6 +13,18 @@ void test_exit(void){}
 #define LED_RED      BIT0
 #define LED_GREEN    BIT6
 
+void led_green(void)
+{
+      P1OUT |= LED_GREEN;                 /**< Switch on green LED */
+      P1OUT &= ~LED_RED;                  /**< Switch off red LED */
+}
+
+void led_red(void)
+{
+      P1OUT &= ~LED_GREEN;                /**< Switch off green LED */
+      P1OUT |= LED_RED;                   /**< Switch on red LED */
+}
+
 int main(void)
 {
    TEST_PUTS("Stop watchdog timer");
@@ -26,17 +38,15 @@ int main(void)
    TEST_PUTS("Starting loop...");
    while(t < 50)
    {
-      P1OUT |= LED_GREEN;                 /**< Switch on green LED */
-      P1OUT &= ~LED_RED;                  /**< Switch off red LED */
+      led_green();
       __delay_cycles(50000);
-
-      P1OUT &= ~LED_GREEN;                /**< Switch off green LED */
-      P1OUT |= LED_RED;                   /**< Switch on red LED */
+      led_red();
       __delay_cycles(50000);
 
       ++t;
    }
    TEST_PUTS("Finished.");
+   led_green();                          /**< Let us end with a green light. */
 
    TEST_EXIT();                          /**< Tell debugger that session can be stopped */
    return 0;
